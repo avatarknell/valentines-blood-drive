e02fa963e3134e0bed748cd8b7f62fd32cbab7cf
Hillary Chemiron <avatarknell@gmail.com>
2025-02-13 02:59:00 +0300

    mobile responsive

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu=document.getElementById("menu");
+const close=document.getElementById("close");
+
+const offScreenMenu=document.querySelector(".off-screenmenu");
+
+menu.addEventListener("click",()=>{
+    offScreenMenu.classList.add("active");
+    menu.style.display="none";
+})
+close.addEventListener("click",()=>{
+    offScreenMenu.classList.remove("active");
+    menu.style.display="initial";
+})
+console.log(offScreenMenu.classList);
+
+
+
+
